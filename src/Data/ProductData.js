eabcdeeec020530a0bff6718936063c8a7b6f959
Robin <rob.blanquart02100@gmail.com>
2022-04-04 18:29:20 +0200

    create product data 1/2

---
+++ src/Data/ProductData.js
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
+
+const productData = [
+  {
+    id: uuidv4(),
+    name: "Datejust 41 mm non portée",
+    year: "2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus magni fuga, ratione cumque adipisci veritatis veniam modi numquam consectetur dolorum exercitationem unde.",
+    price: 17230,
+    size: "Unique",
+  },
+  {
+    id: uuidv4(),
+    name: "Yacht-Master II 44 mm non portée",
+    year: "2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus magni fuga, ratione cumque adipisci veritatis veniam modi numquam consectetur dolorum exercitationem unde.",
+    price: 53123,
+    size: "Unique",
+  },
+  {
+    id: uuidv4(),
+    name: "Datejust 36 mm pre-owned",
+    year: "2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus magni fuga, ratione cumque adipisci veritatis veniam modi numquam consectetur dolorum exercitationem unde.",
+    price: 20178,
+    size: "Unique",
+  },
+  {
+    id: uuidv4(),
+    name: "Day-Date 40 mm pre-owned",
+    year: "2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus magni fuga, ratione cumque adipisci veritatis veniam modi numquam consectetur dolorum exercitationem unde.",
+    price: 73900,
+    size: "Unique",
+  },
+  {
+    id: uuidv4(),
+    name: "Datejust 41 mm non portée",
+    year: "2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus magni fuga, ratione cumque adipisci veritatis veniam modi numquam consectetur dolorum exercitationem unde.",
+    price: 18030,
+    size: "Unique",
+  },
+];
